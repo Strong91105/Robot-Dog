f2cb06741fd9c1c5489b3198587e5210487a785d
Aditya Jyotis <aditya.k.jyotis@gmail.com>
2026-02-05 14:19:46 +0100

    added mvoe forward odom

--- Robot-Dog/perception/perception/step_detection_lidar.py
+++ Robot-Dog/perception/perception/step_detection_lidar.py
@@ -8,9 +8,10 @@ import numpy as np
 import open3d as o3d
 import sensor_msgs_py.point_cloud2 as pc2
 from scipy.spatial.transform import Rotation as R
-from robot_interfaces.msg import Stair
+#from robot_interfaces.msg import Stair
 from numpy.lib import recfunctions as rfn
 from collections import defaultdict
+from cv_bridge import CvBridge
 
 class LidarStepDetector(Node):
     def __init__(self):
