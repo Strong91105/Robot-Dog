58a96946b16ddcc96e6949ed7821a1cbe77ec18c
Aditya Jyotis <74827847+Strong91105@users.noreply.github.com>
2026-02-17 17:32:02 +0100

    Delete Robot-Dog/control/build directory

--- Robot-Dog/control/build/simple_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/nuc-lassie/practical_course/group_ws/src/control/install/simple_control'
--- Robot-Dog/control/build/simple_control/share/simple_control/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/nuc-lassie/practical_course/group_ws/src/control/build/simple_control"
